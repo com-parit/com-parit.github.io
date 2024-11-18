ca85b209efb276936f43792244c1aa979ba3392d
jawad571 <jawadmustafa571@gmail.com>
2024-11-18 00:40:31 +0000

    update docs

---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import { ThemeProvider } from "@/components/contexts/theme-provider";
+import { Navbar } from "@/components/navbar";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import { Footer } from "@/components/footer";
+import "@/styles/globals.css";
+
+export const metadata: Metadata = {
+  title: "Comparit",
+  // metadataBase: new URL("https://ariadocs.vercel.app/"),
+  description:
+    "An IDE Agnostic Model Comparison Tool.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/comparit_logo.svg" />
+      </head>
+      <body
+        className={`${GeistSans.variable} ${GeistMono.variable} font-regular antialiased`}
+        suppressHydrationWarning
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          <main className="sm:container mx-auto w-[90vw] h-auto scroll-smooth">
+            {children}
+          </main>
+          {/* <Footer /> */}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,40 @@
+import { buttonVariants } from "@/components/ui/button";
+import { page_routes } from "@/lib/routes-config";
+import { MoveUpRightIcon, TerminalSquareIcon } from "lucide-react";
+import Link from "next/link";
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="flex sm:min-h-[92vh] min-h-[85vh] flex-col items-center justify-center text-center px-2 py-8">
+      
+      <div className="flex flex-row mb-4 items-center">
+      <Image src="/comparit_logo.svg" width={60} height={60} alt="logo"/>
+      <h1 className="pl-[20px] text-3xl font-bold sm:text-6xl items-center">
+        Comparit
+      </h1>
+      </div>
+      <p className="mb-8 sm:text-xl max-w-[800px] text-muted-foreground">
+        An IDE-agnostic domain model comparison tool
+      </p>
+      <div className="flex flex-row items-center gap-5">
+        <Link
+          href={`/docs${page_routes[0].href}`}
+          className={buttonVariants({ className: "px-6", size: "lg" })}
+        >
+          Get Stared
+        </Link>
+        {/* <Link
+          href="/blog"
+          className={buttonVariants({
+            variant: "secondary",
+            className: "px-6",
+            size: "lg",
+          })}
+        >
+          Read Blog
+        </Link> */}
+      </div>
+    </div>
+  );
+}
