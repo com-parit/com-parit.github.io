bb6fadf90038c72c7b1f29788ddd8b337a98e850
jawad571 <jawadmustafa571@gmail.com>
2024-11-29 23:20:47 +0000

    remove footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -38,20 +38,6 @@ export function Footer() {
 export function FooterButtons() {
   return (
     <>
-      <Link
-        href="https://vercel.com/templates/next.js/documentation-template"
-        className={buttonVariants({ variant: "outline", size: "sm" })}
-      >
-        <TriangleIcon className="h-[0.8rem] w-4 mr-2 text-primary fill-current" />
-        Deploy
-      </Link>
-      <Link
-        href="https://github.com/sponsors/nisabmohd"
-        className={buttonVariants({ variant: "outline", size: "sm" })}
-      >
-        <HeartIcon className="h-4 w-4 mr-2 text-red-600 fill-current" />
-        Sponsor
-      </Link>
     </>
   );
 }
